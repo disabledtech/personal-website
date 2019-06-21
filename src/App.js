75b9e22c1c3c76c65f8abb888fbd2e4c920bd01d
Taylor Danielson <disabledtech@gmail.com>
2019-06-21 15:12:00 -0700

    Updated some links and title

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Header from './components/Header';
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+
+        <Row>
+          <Col>
+            <Header />
+          </Col>
+        </Row>
+        <Row>
+
+          <Col className="text-center zoom mb-3" s={12} md={4}>
+            <a href="www.linkedin.com/in/disabledtech" target="_blank" rel="noopener noreferrer">
+              <i 
+                className="fab fa-linkedin-in fa-5x border border-light rounded-circle p-4"
+                aria-hidden="true" 
+                aria-label="Find me on LinkedIn"
+                title="Find me on LinkedIn"
+              >
+              </i>
+            </a>
+          </Col>
+
+          <Col className="text-center zoom mb-3" s={12} md={4}>
+            <a href="git" target="_blank" rel="noopener noreferrer">
+              <i 
+                className="fas fa-at fa-5x border border-light rounded-circle p-4"
+                aria-hidden="true" 
+                aria-label="Email me"
+                title="Email me"
+              >
+              </i>
+            </a>
+          </Col>
+
+          <Col className="text-center zoom mb-3" s={12} md={4}>
+            <a href="https://github.com/disabledtech" target="_blank" rel="noopener noreferrer">
+              <i 
+                className="fab fa-github fa-5x border border-light rounded-circle p-4" 
+                aria-hidden="true" 
+                aria-label="Find me on Github"
+                title="Find me on Github"
+              >
+              </i>   
+            </a>     
+          </Col>
+
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="text-center header__title">Taylor Danielson</h1>
+            <h5 className="text-center header__subtitle">Web Developer &bull; Python Scripts &bull; UI Design</h5>
+        </header>
+    )
+}
+
+export { Header as default };
